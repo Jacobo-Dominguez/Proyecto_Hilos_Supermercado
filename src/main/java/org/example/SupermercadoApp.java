326c929d9f30c43fcb4fcd809cbf2cc34db8b27a
Jacobo-Dominguez <jacobo9810@gmail.com>
2026-02-11 18:08:23 +0100

    Clase main

--- src/main/java/org/example/SupermercadoApp.java
+++ src/main/java/org/example/SupermercadoApp.java
@@ -1,17 +1,54 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class SupermercadoApp {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import org.example.concurrencia.Caja;
+import org.example.concurrencia.GeneradorClientes;
+import org.example.metricas.Estadisticas;
+import org.example.model.Cliente;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class SupermercadoApp {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        int numeroCajas = 3;
+        int capacidadCola = 50;
+        int duracionSimulacion = 10000; // 10 segundos
+
+        BlockingQueue<Cliente> cola = new ArrayBlockingQueue<>(capacidadCola);
+        Estadisticas estadisticas = new Estadisticas();
+        AtomicInteger contadorId = new AtomicInteger();
+
+        ExecutorService executor = Executors.newFixedThreadPool(numeroCajas + 1);
+
+        long inicio = System.currentTimeMillis();
+
+        // Generador
+        executor.execute(new GeneradorClientes(
+                cola, estadisticas, duracionSimulacion, contadorId));
+
+        // Cajas
+        List<Caja> cajas = new ArrayList<>();
+
+        for (int i = 1; i <= numeroCajas; i++) {
+            Caja caja = new Caja(i, cola, estadisticas);
+            cajas.add(caja);
+            executor.execute(caja);
         }
+
+        Thread.sleep(duracionSimulacion);
+
+        cajas.forEach(Caja::detener);
+
+        executor.shutdown();
+        executor.awaitTermination(1, TimeUnit.MINUTES);
+
+        long fin = System.currentTimeMillis();
+
+        estadisticas.mostrarInforme(fin - inicio);
     }
 }
