cbe56f5b46296f17e880d22b03c0c7bdc3c2833a
Jacobo-Dominguez <jacobo9810@gmail.com>
2026-02-11 18:05:26 +0100

    Clase modelo (Representa cliente del supermercado)

--- src/main/java/org/example/model/Cliente.java
+++ src/main/java/org/example/model/Cliente.java
@@ -1,4 +1,36 @@
 package org.example.model;
 
 public class Cliente {
+
+    private final int id;
+    private final int numeroArticulos;
+    private final long instanteLlegada;
+    private long instanteInicioAtencion;
+    private long instanteSalida;
+
+    public Cliente(int id, int numeroArticulos) {
+        this.id = id;
+        this.numeroArticulos = numeroArticulos;
+        this.instanteLlegada = System.currentTimeMillis();
+    }
+
+    public int getId() { return id; }
+    public int getNumeroArticulos() { return numeroArticulos; }
+    public long getInstanteLlegada() { return instanteLlegada; }
+
+    public void setInstanteInicioAtencion(long instanteInicioAtencion) {
+        this.instanteInicioAtencion = instanteInicioAtencion;
+    }
+
+    public void setInstanteSalida(long instanteSalida) {
+        this.instanteSalida = instanteSalida;
+    }
+
+    public long getTiempoEspera() {
+        return instanteInicioAtencion - instanteLlegada;
+    }
+
+    public long getTiempoAtencion() {
+        return instanteSalida - instanteInicioAtencion;
+    }
 }
